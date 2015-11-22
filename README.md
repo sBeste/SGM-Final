8835da14bf5fa553d6acdcf9413cc397af3c7a99
huterdamm <huterdamm@yahoo.de>
2015-11-22 20:50:17 +0100

    Klassen und so aus dem alten repository SGM-NEW übernommen

---
+++ README.md
@@ -0,0 +1,5 @@
+# SGM
+
+[![Build Status](https://travis-ci.org/sBeste/SGM-new.svg?branch=master)](https://travis-ci.org/sBeste/SGM-new)
+
+Schulprojekt zum lernen von C#
---
+++ SGM-Final/SGM-Final/Class/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SGM
+{
+	public class Customer
+	{
+		public string Name {get; private set;}
+		public string FirstName {get; private set;}
+		public double CreditBalance {get; private set;}
+		public List<Order> OrderList;
+
+		public Customer (string FirstName, string Name, double CreditBalance)
+		{
+			this.FirstName = FirstName;
+			this.Name = Name;
+			this.CreditBalance = CreditBalance;
+		}
+
+		public double pay (double amount) {
+			this.CreditBalance = this.CreditBalance - amount;
+
+			return this.CreditBalance;
+		}
+	}
+}
