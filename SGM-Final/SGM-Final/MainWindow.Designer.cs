58259796f6e3315bcd71fe455959afbd3670f8eb
huterdamm <huterdamm@yahoo.de>
2015-11-22 20:57:21 +0100

    From1 in MainWindow geändert und button als test hinzugefügt

--- SGM-Final/SGM-Final/MainWindow.Designer.cs
+++ SGM-Final/SGM-Final/MainWindow.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SGM_Final
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 411);
+            this.Controls.Add(this.button1);
+            this.Name = "MainWindow";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
