6036744685ca6c426cce677d6614325973cb5d01
Jannick Fahlbusch <git@jf-projects.de>
2015-11-24 10:22:51 +0100

    Umbenennung von button1 in addCustomerButton

--- SGM-Final/SGM-Final/addCustomerWindow.Designer.cs
+++ SGM-Final/SGM-Final/addCustomerWindow.Designer.cs
@@ -34,7 +34,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.addCustomerButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -87,21 +87,21 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "Budget";
             // 
-            // button1
+            // addCustomerButton
             // 
-            this.button1.Location = new System.Drawing.Point(511, 414);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(161, 49);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "hinzufügen";
-            this.button1.UseVisualStyleBackColor = true;
+            this.addCustomerButton.Location = new System.Drawing.Point(511, 414);
+            this.addCustomerButton.Name = "addCustomerButton";
+            this.addCustomerButton.Size = new System.Drawing.Size(161, 49);
+            this.addCustomerButton.TabIndex = 7;
+            this.addCustomerButton.Text = "hinzufügen";
+            this.addCustomerButton.UseVisualStyleBackColor = true;
             // 
             // addCustomerWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1215, 755);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.addCustomerButton);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -123,6 +123,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button addCustomerButton;
     }
 }
