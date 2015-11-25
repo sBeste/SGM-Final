b9d72f018952fc36fe98ee20c247ae8ca8f812d4
NgCongMinh <azn_boy_vn@hotmail.de>
2015-11-25 21:55:05 +0100

    add Garnish-Class

---
+++ SGM-Final/SGM-Final/Class/Garnish.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SGM_Final
+{
+	public class Garnish : Product
+	{
+		public static Garnish POMMES = new Garnish("Pommes", 1.0);
+		public static Garnish SALAd = new Garnish("Salat", 1.0);
+
+		private String name;
+		private double price;
+
+		private Garnish (String name, double price)
+		{
+			this.name = name;
+			this.price = price;
+		}
+
+		String Product.getName()
+		{
+			return this.name;
+		}
+
+		double Product.getPrice()
+		{
+			return this.price;
+		}
+	}
+}
+
