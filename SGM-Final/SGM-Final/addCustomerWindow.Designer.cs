c13e0d68f7100703f9c83fbbbcb4c3aaffbe0876
Jannick Fahlbusch <git@jf-projects.de>
2015-12-17 13:05:24 +0100

    Remove unused fields

--- SGM-Final/SGM-Final/addCustomerWindow.Designer.cs
+++ SGM-Final/SGM-Final/addCustomerWindow.Designer.cs
@@ -113,10 +113,6 @@
 
 		#endregion
 
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.TextBox textBox2;
-		private System.Windows.Forms.TextBox textBox3;
-		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Button addCustomerButton;
